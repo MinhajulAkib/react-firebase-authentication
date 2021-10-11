6cc4c18c2441c6b0717c9b85e6eb739af09bad75
MinhajulAkib <00akibswe@gmail.com>
2021-10-11 20:22:04 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCV3tgjdT-uMvMZGUZpKMHq1DRCA_YS1xo",
+    authDomain: "react-firebase-authentic-ffba7.firebaseapp.com",
+    projectId: "react-firebase-authentic-ffba7",
+    storageBucket: "react-firebase-authentic-ffba7.appspot.com",
+    messagingSenderId: "113948748352",
+    appId: "1:113948748352:web:4a847a8248c23e1990cda5"
+  };
+
+  export default firebaseConfig;
